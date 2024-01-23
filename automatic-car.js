9408e5671e072a4ea91d3f1c3534e0e54d87972e
David Urting <David.Urting@lv.vlaanderen.be>
2024-01-23 13:38:17 +0100

    Aanpassingen na feedback hoofdstuk 2 (modules)

--- automatic-car.js
+++ automatic-car.js
@@ -1,19 +1,25 @@
 class AutomaticCar extends Car { // overerven gebeurt via het ‘extends’ keyword
     #gearMode; // ter info: mogelijke waarden zijn Park, Reverse, Neutral, Drive, ...
-    constructor(brand, color, fuelType) {
-      super(brand, color, fuelType); // verplicht: super roept de constructor van parent class Car aan.
-      this.#gearMode = "Park";
+    constructor(brand, color, maxSpeed) {
+      super(brand, color, maxSpeed); // verplicht: super roept de constructor van parent class Car aan.
+      this.#gearMode = "P";
     }
     get gearMode() { // Properties beginnen ook met een kleine letter (= conventie)
         return this.#gearMode; 
     } 
-    set gearMode(newGearMode) { // Een ‘property setter’ voor schrijftoegang.
-        this.#gearMode = newGearMode; 
+    set gearMode(newValue) { // Een ‘property setter’ voor schrijftoegang.
+        if (newValue !== "P"  && newValue !== "N"  && newValue !== "R"  && newValue !== "D")
+            throw "Je moet kiezen voor P, N, R of D. ";
+
+        this.#gearMode = newValue; 
         console.log(`Gear mode of automatic ${this.brand} with ID ${this.id} has been set to ${this.#gearMode}.`);
     } 
-    move(pedalPosition) {     
-        // Berekenen van de gear ...
+    move() {
+        // Berekenen van de nieuwe gear position ...
         console.log(`Calculating recommended gear position of automatic ${this.brand} with ID ${this.id}...`);
-        super.move(pedalPosition);
+        if (this.gear < 5) {
+            this.gearUp();
+        }        
+        super.move(); // een object methode op de parent roep je aan met super
     } 
   } 
--- car.js
+++ car.js
@@ -1,42 +1,61 @@
-class Car { // de naam begint met een hoofdletter (= conventie)
-    static #lastId = 0;
+class Car { // een class naam begint steeds met een hoofdletter (= conventie)
+    static #lastId = 0; // static geeft aan dat dit een class field is  en dus geen object field
 
-    #id;  // De hashtag (#) geeft aan dat dit ‘private’ attributen zijn
+    #id;  // De hashtag (#) geeft aan dat dit private object fields zijn
     #brand;
     #color;
-    #fuelType;
+    #maxSpeed;
+
+    #started;
     #gear;
-    #speed;
+    #acceleratorPedalPosition;
+    #brakePedalPosition;
 
-    constructor(brand, color, fuelType) { // slechts één constructor mogelijk. Bemerk het gebruik van this.
+    // slechts één constructor mogelijk. Bemerk het gebruik van this.
+    constructor(brand, color, maxSpeed) { 
       this.#id = Car.#lastId++;
-
       this.#brand = brand; 
       this.#color = color; 
-      this.#fuelType = fuelType; 
-      this.#gear = 0; 
-      this.#speed = 0;
-    }
+      this.#maxSpeed = maxSpeed; 
 
+      this.#started = false; 
+      this.#gear = 0; 
+      this.#acceleratorPedalPosition = 0; // ter info: position is een waarde tussen 0 en 1
+      this.#brakePedalPosition = 0; // ter info: position is een waarde tussen 0 en 1
+      }
+
+    // Properties beginnen ook met een kleine letter (= conventie)
     get id() { return this.#id; }
     get brand() { return this.#brand; }
+    get started() { return this.#started; } 
     get gear() { return this.#gear; } 
+    get acceleratorPedalPosition() { return this.#acceleratorPedalPosition; }
+    set acceleratorPedalPosition(newValue) {
+        if (newValue < 0 || newValue > 1) 
+            throw "Gelieve een waarde (kommagetal) in het interval [0,1] te kiezen (1 = volledig ingedrukt)";
+        this.#acceleratorPedalPosition = newValue;
+    }
+    get brakePedalPosition() { return this.#brakePedalPosition; }
+    set brakePedalPosition(newValue) {
+        if (newValue < 0 || newValue > 1) 
+            throw "Gelieve een waarde (kommagetal) in het interval [0,1] te kiezen (1 = volledig ingedrukt)";
+        this.#brakePedalPosition = newValue;
+    }
 
-    start() { // method van een Car object. De naam begint met een kleine letter (= conventie)
+    // method van een Car object. De naam begint met een kleine letter (= conventie)
+    start() { 
+        this.#started = true;
         console.log(`${this.#brand} with ID ${this.id} started.`);
     }  
 
     stop() {
+        this.#started = stop;
         console.log(`${this.#brand} with ID ${this.id} stopped.`);
     } 
 
-    move(pedalPosition) { // ter info: pedalPosition is een percentage (dus number tussen 0 en 100).
+    move(pedalPosition) { 
         console.log(`Speed pedal position of ${this.#brand} with ID ${this.id} is ${pedalPosition}%.`);
     } 
-
-    brake(pedalPosition) {
-        console.log(`Brake pedal position of ${this.#brand} with ID ${this.id} is ${pedalPosition}%.`);
-    }
 
     gearUp() {        
         this.#gear++;
@@ -46,5 +65,13 @@ class Car { // de naam begint met een hoofdletter (= conventie)
     gearDown() {
         this.#gear--;
         console.log(`Gear down. Gear position of ${this.#brand} with ID ${this.id} is ${this.gear}.`);
-    }    
+    }
+
+    move() {
+        if (this.#started) {
+            console.log(`Moving ${this.#brand} with ID ${this.id}: gear=${this.gear}, accelerator (%)=${this.#acceleratorPedalPosition * 100}, brake (%)=${this.#brakePedalPosition * 100}.`);
+        } else {
+            console.log(`${this.#brand} with ID ${this.id} is not started.`);
+        }        
+    }
   }
